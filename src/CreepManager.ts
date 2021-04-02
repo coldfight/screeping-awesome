30b2c5ff0f5c243d57248342847852820ca9be57
Amedeo Bonanno <coldfight@live.com>
2021-04-02 07:16:46 -0400

    Tutorial 1 - Spawn a creep

--- src/CreepManager.ts
+++ src/CreepManager.ts
@@ -1,5 +1,17 @@
 export class CreepManager {
   public run(): void {
-    console.log("Lets run");
+    if (this.shouldSpawnHarvester("Harvester1")) {
+      console.log("SPAWNING HARVESTER1");
+      this.spawnHarvester("Spawn1", "Harvester1");
+    }
+  }
+
+  private shouldSpawnHarvester(creepName: string): boolean {
+    return !Game.creeps[creepName];
+  }
+
+  private spawnHarvester(spawnName: string, creepName: string): void {
+    // Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+    Game.spawns[spawnName].spawnCreep([WORK, CARRY, MOVE], creepName);
   }
 }
