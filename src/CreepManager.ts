39dbb2f9f8dc25d4cd9823e1df130e997988499d
Amedeo Bonanno <coldfight@live.com>
2021-04-02 07:25:26 -0400

    Tutorial 2 - Harvest energy

--- src/CreepManager.ts
+++ src/CreepManager.ts
@@ -1,9 +1,10 @@
 export class CreepManager {
   public run(): void {
     if (this.shouldSpawnHarvester("Harvester1")) {
-      console.log("SPAWNING HARVESTER1");
       this.spawnHarvester("Spawn1", "Harvester1");
     }
+
+    this.harvestEnergy("Harvester1");
   }
 
   private shouldSpawnHarvester(creepName: string): boolean {
@@ -11,7 +12,18 @@ export class CreepManager {
   }
 
   private spawnHarvester(spawnName: string, creepName: string): void {
-    // Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+    console.log(`> Spawning Harvester: ${creepName}`);
     Game.spawns[spawnName].spawnCreep([WORK, CARRY, MOVE], creepName);
+  }
+
+  private harvestEnergy(creepName: string): void {
+    const creep = Game.creeps[creepName];
+    const sources = creep.room.find(FIND_SOURCES);
+    if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+      console.log(`> Moving '${creepName}' to source to harvest energy`);
+      creep.moveTo(sources[0]);
+    } else {
+      console.log(`> '${creepName}' is harvesting energy`);
+    }
   }
 }
